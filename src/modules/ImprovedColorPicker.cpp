32b99c28c17c5941994766aca64ef9223964a047
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-07-28 05:53:25 -0400

    Goofy hijack

--- src/modules/ImprovedColorPicker.cpp
+++ src/modules/ImprovedColorPicker.cpp
@@ -4,6 +4,7 @@
 #include "utils/NextFree/NextFreeProvider.hpp"
 #include "utils/NextFree/sources/ColorSource.hpp"
 #include "utils/NextFree/NextFreeOffsetInput.hpp"
+#include "utils/Utils.hpp"
 
 bool ImprovedColorPicker::onToggled(bool state) {
     return true;
@@ -376,6 +377,24 @@ bool ICPCustomizeObjectLayer::init(GameObject* obj, CCArray* objs) {
     }
     if (auto menu = m_mainLayer->getChildByID("copy-paste-menu")) {
         menu->setPositionY(winSize.height / 2.f + 50.f);
+    }
+
+    auto okMenu = m_mainLayer->getChildByID("ok-menu");
+    if (okMenu) {
+        auto okBtn = static_cast<CCMenuItemSpriteExtra*>(okMenu->getChildByID("ok-button"));
+        tinker::utils::hijackButton(okBtn, [this] (std::function<void(CCObject* sender)> orig, CCObject* sender) {
+            m_customColorInput->m_delegate = nullptr;
+            m_customColorInput->onClickTrackNode(false);
+            
+            if (m_textInput) {
+                m_textInput->onClickTrackNode(false);
+                m_textInput->m_delegate = nullptr;
+            }
+            GameManager::get()->m_currentColorChannel = m_customColorChannel;
+            EditorUI::get()->updateObjectInfoLabel();
+            setKeypadEnabled(false);
+            removeFromParent();
+        });
     }
 
     auto channelsMenu = m_mainLayer->getChildByID("channels-menu");
