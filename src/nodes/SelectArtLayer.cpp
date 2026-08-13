bcf86772a5f9b183216506d3f6344de66a7d3059
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-08-13 11:33:51 -0400

    Update SelectArtLayer.cpp

--- src/nodes/SelectArtLayer.cpp
+++ src/nodes/SelectArtLayer.cpp
@@ -451,10 +451,10 @@ void SelectArtLayer::keyBackClicked() {
         m_levelSettingsLayer->m_middlegroundSprite->setDisplayFrame(newSpr);
         m_editorLayer->createMiddleground(m_editorLayer->m_levelSettings->m_middleGroundIndex);
     }
-    m_artNodes.clear();
     m_scrollLayer->getContentLayer()->removeAllChildren();
     m_scrollLayer->removeFromParent();
     m_scrollLayer = nullptr;
+    m_artNodes.clear();
     Popup::keyBackClicked();
 }
 
