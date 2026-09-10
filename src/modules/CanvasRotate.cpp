a952a3f12f340ee278c0aec4cb9775a6fce30d42
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-09-10 12:37:06 -0400

    Update CanvasRotate.cpp

--- src/modules/CanvasRotate.cpp
+++ src/modules/CanvasRotate.cpp
@@ -474,8 +474,6 @@ OBB2D* CRLevelEditorLayer::rotatedOBB2D(GameObject* object, CCPoint pivot, float
     return OBB2D::create(rotatedCenter, width, height, radians);
 }
 
-#ifndef GEODE_IS_IOS
-
 void CRCCTextInputNode::updateCursorPosition(cocos2d::CCPoint position, cocos2d::CCRect rect) {
     auto transform = nodeToWorldTransform();
 
@@ -489,5 +487,3 @@ void CRCCTextInputNode::updateCursorPosition(cocos2d::CCPoint position, cocos2d:
 
     CCTextInputNode::updateCursorPosition(position, {});
 }
-
-#endif
